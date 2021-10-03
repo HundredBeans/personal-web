ee4cc61a68f5e498d2cb53b9eede59eca22fc03d
Mohammad Daffa <m.daffa100@yahoo.com>
2021-10-03 08:09:00 +0700

    Finsihed Home Page (temporary)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
-const Index = () => <h1>Hello World</h1>;
+import { Container } from '@chakra-ui/react';
+import { Hero } from '../components/Hero';
+
+const Index = () => {
+  const TextChildren = () => (
+    <>
+      I am a <b>Software Engineer</b>, <b>Web Developer</b>, and a <b>Lifelong Learner</b> with a
+      background of <b>Petroleum and Geothermal Engineering</b>.
+    </>
+  );
+  return (
+    <Container>
+      <Hero headingText="Hello There!" textChildren={<TextChildren />} height="100vh"></Hero>
+    </Container>
+  );
+};
 
 export default Index;
